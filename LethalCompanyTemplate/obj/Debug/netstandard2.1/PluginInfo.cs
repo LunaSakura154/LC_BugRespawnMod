15990401d616c64384618b1b58545d1819a3bbb8
LunaBroeke <onefivefoursakura@gmail.com>
2023-12-14 21:34:59 +0100

    Setup

---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalCompanyTemplate
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LC_BugRespawnMod";
+        public const string PLUGIN_NAME = "LC_BugRespawnMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}

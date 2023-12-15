d735726defa3ebe93ab8ab79d13f3cec7e9f679a
LunaBroeke <onefivefoursakura@gmail.com>
2023-12-15 13:53:52 +0100

    Added Bug Spawning and Finding Bug Index

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,42 +1,66 @@
 ﻿using BepInEx;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using LC_API.GameInterfaceAPI;
 using UnityEngine;
 using Unity.Netcode;
 using HarmonyLib;
 using GameNetcodeStuff;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HoarderMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        List<int> hoardID = new();
         HoarderBugAI[] bugs;
         RoundManager curRound;
+        GameObject player;
+        SelectableLevel curLevel;
+        int hoardIndex;
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            GameState.LandOnMoon += SpawnBug;
+            GameState.LandOnMoon += RoundStart;
         }
 
-        private void SpawnBug()
+        private void RoundStart()
         {
             curRound = FindObjectOfType<RoundManager>();
-            Logger.LogInfo($"{curRound.gameObject.name}");
+            curLevel = curRound.currentLevel;
+            Logger.LogInfo($"{curLevel.PlanetName}");
             var pcb = FindObjectsOfType<PlayerControllerB>();
-            GameObject player = GameObject.Find("Player");
+            player = GameObject.Find("Player");
             Logger.LogInfo($" Current Player: {player.name} - {player.transform.position} - {player.layer} {player.tag} {ListAllComponents(player)}");
             for (int i = 0; i < pcb.Length; i++)
             {
                 Logger.LogInfo($"{pcb[i].gameObject.name} - {pcb[i].transform.position}");
             }
+            hoardIndex = FindBugID("Hoarding bug");
+            BugCheck();
         }
 
-        private void FixedUpdate()
+        private async Task BugCheck()
         {
-            bugs = FindObjectsOfType<HoarderBugAI>();
-            foreach (var b in bugs) { Logger.LogInfo($"{b.name} - {b.transform.position} - {b.nestPosition} - {b.isEnemyDead}"); }
+            while (true)
+            {
+                bugs = FindObjectsOfType<HoarderBugAI>();
+                foreach (var b in bugs) 
+                { 
+                    Logger.LogInfo($"{b.name} - {b.transform.position} - {b.nestPosition} - {b.isEnemyDead} - {b.GetInstanceID()}");
+                    Logger.LogInfo(hoardID.Contains(b.GetInstanceID()));
+                    if (b.isEnemyDead == true && hoardID.Contains(b.GetInstanceID()) == false)
+                    {
+                        for (int i = 0; i < 5; i++){ curRound.SpawnEnemyOnServer(b.transform.position, 0, 2); }
+                        hoardID.Add(b.GetInstanceID());
+                    }
+                }
+                
+                await Task.Delay(10);
+            }
         }
 
         public string ListAllComponents(GameObject go)
@@ -45,6 +69,22 @@ namespace HoarderMod
             Component[] comp = go.GetComponents<Component>();
             foreach (Component c in comp) { t.Add(c.GetType().ToString()); }
             return string.Join(',', t);
+        }
+
+        public int FindBugID(string enemy)
+        {
+            for (int i = 0; i < curLevel.Enemies.Count; i++)
+            {
+                SpawnableEnemyWithRarity e = curLevel.Enemies[i];
+                if (e.enemyType.enemyName == enemy)
+                {
+                    Logger.LogInfo(e.enemyType.enemyName);
+                    Logger.LogInfo($"Found {enemy} at {i}");
+                    return i;
+                }
+            }
+            Logger.LogInfo($"{enemy} index not found, probably doesn't spawn in that case but I'm defaulting to 2");
+            return 2;
         }
     }
 }
